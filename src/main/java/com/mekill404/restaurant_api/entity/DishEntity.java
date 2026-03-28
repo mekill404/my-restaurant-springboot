ba34cb24443a00e03cc9cdcccb2459f27f45caf1
mekill404 <hei.mahery.55@gmail.com>
2026-03-28 12:25:30 +0300

    refactor(entity): ajouter l'annotation @NoArgsConstructor à DishEntity -Ajout de l'annotation Lombok @NoArgsConstructor à la classe DishEntity. -Cette modification permet l'instanciation de l'entité par des frameworks nécessitant un constructeur sans argument (comme Hibernate/JPA ou Jackson pour la désérialisation JSON)

--- src/main/java/com/mekill404/restaurant_api/entity/DishEntity.java
+++ src/main/java/com/mekill404/restaurant_api/entity/DishEntity.java
@@ -6,9 +6,11 @@ import com.mekill404.restaurant_api.entity.enums.DishTypeEnum;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DishEntity {
     private int id;
     private String name;
