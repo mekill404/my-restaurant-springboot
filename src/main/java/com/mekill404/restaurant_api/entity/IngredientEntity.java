81ebf22cf54681bd8d618c205fb14fae4ab4ab31
mekill404 <hei.mahery.55@gmail.com>
2026-03-28 12:26:40 +0300

    refactor(entity): ajout de @NoArgsConstructor IngredientEntity.java - Ajout de l'annotation Lombok @NoArgsConstructor pour permettre l'instanciation de l'entité par les frameworks.

--- src/main/java/com/mekill404/restaurant_api/entity/IngredientEntity.java
+++ src/main/java/com/mekill404/restaurant_api/entity/IngredientEntity.java
@@ -1,18 +1,17 @@
 package com.mekill404.restaurant_api.entity;
 
-import java.util.List;
-
 import com.mekill404.restaurant_api.entity.enums.CategoryEnum;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class IngredientEntity {
     private int id;
     private String name;
     private Double price;
     private CategoryEnum category;
-    private List<StockMovement> stockMovements;
 }
