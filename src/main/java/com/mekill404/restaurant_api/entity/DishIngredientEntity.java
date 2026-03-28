398988e41c9b54f673c277c3acc901acc2aaf971
mekill404 <hei.mahery.55@gmail.com>
2026-03-28 12:26:30 +0300

    refactor(entity): ajout de @NoArgsConstructor et @JsonIgnore à DishIngredientEntity - Ajout de l'annotation Lombok @NoArgsConstructor pour permettre l'instanciation de l'entité par les frameworks (JPA/Jackson).Ajout de l'annotation @JsonIgnore sur le champ dish afin d'éviter les boucles de récursion infinie lors de la sérialisation JSON (relation bidirectionnelle).

--- src/main/java/com/mekill404/restaurant_api/entity/DishIngredientEntity.java
+++ src/main/java/com/mekill404/restaurant_api/entity/DishIngredientEntity.java
@@ -1,14 +1,18 @@
 package com.mekill404.restaurant_api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mekill404.restaurant_api.entity.enums.UnitEnum;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DishIngredientEntity {
     private Integer id;
+    @JsonIgnore
     private DishEntity dish;
     private IngredientEntity ingredient;
     private Double quantity;
